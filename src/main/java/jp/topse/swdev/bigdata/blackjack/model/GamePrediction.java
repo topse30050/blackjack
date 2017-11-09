e6ed0fd7beb7e41e0f7c6bf3ad29064cf4ba11dd
Suguru Kusanagi <nagi_su@yahoo.co.jp>
2017-11-09 15:12:34 +0900

    added readmodel

---
+++ src/main/java/jp/topse/swdev/bigdata/blackjack/model/GamePrediction.java
@@ -0,0 +1,18 @@
+package jp.topse.swdev.bigdata.blackjack.model;
+/**
+ * @author nagi
+ *
+ */
+public class GamePrediction {
+
+//	private static final String ARFF_PATH= "src/main/resources/iris.arff";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test");
+		
+	}
+
+	
+}
