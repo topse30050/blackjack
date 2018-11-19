7bd573e1c744e5e74b8180176fa943c5682ce2df
topse30050 <hiromasa_mori@cm.jip.co.jp>
2018-11-19 17:47:17 +0900

    BUST予測と勝敗予測の2モデル併用型にしてみた。

--- src/main/java/jp/topse/swdev/bigdata/blackjack/topse30050/Topse30050AI.java
+++ src/main/java/jp/topse/swdev/bigdata/blackjack/topse30050/Topse30050AI.java
@@ -6,11 +6,12 @@ public class Topse30050AI implements DecisionMaker {
 
     /**
      * ゲーム方針は下記の通り。<br/>
-     * 学習結果モデルに
-     *
-     * を入力し
-     *
-     * する。
+     * <ol>
+     * <li>手持ちが5枚を越えている場合は初期状態の合計点数17未満ならHITの思考とする</li>
+     * <li>学習結果モデル(BUST判定)に手持ちのカード(5枚不足分は0とする)を入力しBUSTしそうならSTANDする</li>
+     * <li>学習結果モデル(勝敗判定)にディーラーのUPカード、手持ちのカード(5枚不足分は0とする)を入力し、
+     *     今の状態で合計点数が12未満または予測が負けの場合はHIT、それ以外はSTANDする。</li>
+     * </ol>
      */
     @Override
     public Action decide(Player player, Game game) {
@@ -18,28 +19,6 @@ public class Topse30050AI implements DecisionMaker {
         Hand myHand = game.getPlayerHands().get(player);
         int nowTotal = myHand.eval();
         int nowCardCount = myHand.getCount();
-
-//        System.out.println();
-//        System.out.println("ディーラーのアップカード:" + game.getUpCard());
-//        System.out.println(player.getName() + "の今の手持ち合計:" + nowTotal);
-//        System.out.println(player.getName() + "の今の手持ち枚数:" + nowCardCount);
-//        String separator = "";
-//        for (int i = 0; i < myHand.getCount(); i++) {
-//            System.out.print(separator + myHand.get(i));
-//            separator = ",";
-//        }
-//        System.out.println("の最後のカード:" + myHand.get(myHand.getCount() - 1));
-//        System.out.println("今のカードを引く前の合計:" + ((myHand.getCount() == 2) ? 0 : (myHand.eval() - myHand.get(myHand.getCount() - 1).getValue())));
-//        System.out.println();
-//        game.getPlayerHands().forEach((player1, hand) -> {
-//            System.out.println(player1.getName() + "の今の手持ち合計:" + hand.eval());
-//            String separator = "";
-//            for (int i = 0; i < hand.getCount(); i++) {
-//                System.out.print(separator + hand.get(i));
-//                separator = ",";
-//            }
-//            System.out.println();
-//        });
 
         if (nowCardCount > 5) {
             return (nowTotal < 17) ? Action.HIT : Action.STAND;
@@ -52,6 +31,12 @@ public class Topse30050AI implements DecisionMaker {
         for (int i = nowCardCount; i < cardIndexes.length; i++) {
             cardIndexes[i] = 0;
         }
+
+        Boolean isBust = Topse30050Model.isBust(cardIndexes[0], cardIndexes[1], cardIndexes[2], cardIndexes[3], cardIndexes[4]);
+        if (isBust == null || isBust) {
+            return Action.STAND;
+        }
+
         Result.Type predictedResult = Topse30050Model.getPredictedResult(
                 game.getUpCard().getIndex(),
                 cardIndexes[0],
@@ -59,8 +44,6 @@ public class Topse30050AI implements DecisionMaker {
                 cardIndexes[2],
                 cardIndexes[3],
                 cardIndexes[4]);
-//System.out.println("今の状態での勝敗予想:" + predictedResult);
-//System.out.println(((predictedResult == null || predictedResult.equals(Result.Type.WIN)) ? Action.STAND : Action.HIT).toString() + "する");
         if (predictedResult == null) {
             return Action.STAND;
         } else if (predictedResult.equals(Result.Type.LOSE) || nowTotal < 12) {
@@ -68,8 +51,6 @@ public class Topse30050AI implements DecisionMaker {
         } else {
             return Action.STAND;
         }
-
-//        return (game.getPlayerHands().get(player).eval() < 17) ? Action.HIT : Action.STAND;
     }
 
 }
--- src/main/java/jp/topse/swdev/bigdata/blackjack/topse30050/Topse30050Model.java
+++ src/main/java/jp/topse/swdev/bigdata/blackjack/topse30050/Topse30050Model.java
@@ -1,10 +1,11 @@
 package jp.topse.swdev.bigdata.blackjack.topse30050;
 
 import jp.topse.swdev.bigdata.blackjack.Card;
+import jp.topse.swdev.bigdata.blackjack.Hand;
 import jp.topse.swdev.bigdata.blackjack.Result;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.classifiers.trees.RandomForest;
+import weka.classifiers.functions.LibSVM;
 import weka.core.*;
 
 import java.io.File;
@@ -12,7 +13,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class Topse30050Model {
@@ -20,19 +23,25 @@ public class Topse30050Model {
     private static final String TRAIN_DATA = "data/training-deck3.csv";
 //    private static final String TRAIN_ARFF = "data/train.arff";
 //    private static final String EVAL_ARFF = "data/eval.arff";
-    private static final String CLASSIFIER_MODEL = "models/topse30050.model";
+    private static final String CLASSIFIER_MODEL_RESULT = "models/topse30050_result.model";
+    private static final String CLASSIFIER_MODEL_BUST = "models/topse30050_bust.model";
 
-    private static final Result.Type[] CLASS_VALUES = new Result.Type[]{
+    private static final Result.Type[] CLASS_VALUES_RESULT = new Result.Type[]{
             Result.Type.WIN,
             Result.Type.DRAW,
             Result.Type.LOSE,
     };
-    private static Classifier aiModel;
+    private static final String[] CLASS_VALUES_BUST = new String[]{
+            "NON BUST",
+            "BUST",
+    };
+    private static Classifier aiModelResult;
+    private static Classifier aiModelBust;
 
     private Topse30050Model() {
     }
 
-    private static class DataModel {
+    private static class DataModelResult {
         private int dealerUpCard;   // ディーラーの1枚目
         private int hand1;          // 最初に配られた1枚目
         private int hand2;          // 最初に配られた2枚目
@@ -41,7 +50,7 @@ public class Topse30050Model {
         private int hand5;          // HIT3枚目(引いてなければ0)
         private Result.Type result; // 結果
 
-        DataModel(int dealerUpCard, int hand1, int hand2, int hand3, int hand4, int hand5, Result.Type result) {
+        DataModelResult(int dealerUpCard, int hand1, int hand2, int hand3, int hand4, int hand5, Result.Type result) {
             this.dealerUpCard = dealerUpCard;
             this.hand1 = hand1;
             this.hand2 = hand2;
@@ -51,29 +60,58 @@ public class Topse30050Model {
             this.result = result;
         }
     }
+    private static class DataModelBust {
+        private int hand1;          // 最初に配られた1枚目
+        private int hand2;          // 最初に配られた2枚目
+        private int hand3;          // HIT1枚目(引いてなければ0)
+        private int hand4;          // HIT2枚目(引いてなければ0)
+        private int hand5;          // HIT3枚目(引いてなければ0)
+        private String isBustStr;   // BUSTしたかどうか(文字列)
+
+        DataModelBust(int hand1, int hand2, int hand3, int hand4, int hand5, String isBustStr) {
+            this.hand1 = hand1;
+            this.hand2 = hand2;
+            this.hand3 = hand3;
+            this.hand4 = hand4;
+            this.hand5 = hand5;
+            this.isBustStr = isBustStr;
+        }
+    }
+
+    private static int getHandEval(Card[] cards) {
+        Hand hand = new Hand();
+        Arrays.stream(cards).filter(Objects::nonNull).forEach(hand::add);
+        return hand.eval();
+    }
 
     private static void makeClassifierModel() {
-        List<DataModel> data= new ArrayList<>();
+        List<DataModelResult> dataResult= new ArrayList<>();
+        List<DataModelBust> dataBust = new ArrayList<>();
         try (Stream<String> line = Files.lines(Paths.get(TRAIN_DATA))) {
             line.map(str -> str.split(",")).forEach(elements -> {
-                Card[] dealerHands = {Card.valueOf(elements[1]), Card.valueOf(elements[2]), (elements[3].length() == 0) ? null : Card.valueOf(elements[3]), (elements[4].length() == 0) ? null : Card.valueOf(elements[4]), (elements[5].length() == 0) ? null : Card.valueOf(elements[5])};
-                Card[] p1Hands = {Card.valueOf(elements[7]), Card.valueOf(elements[8]), (elements[9].length() == 0) ? null : Card.valueOf(elements[9]), (elements[10].length() == 0) ? null : Card.valueOf(elements[10]), (elements[11].length() == 0) ? null : Card.valueOf(elements[11])};
-                Card[] p2Hands = {Card.valueOf(elements[14]), Card.valueOf(elements[15]), (elements[16].length() == 0) ? null : Card.valueOf(elements[16]), (elements[17].length() == 0) ? null : Card.valueOf(elements[17]), (elements[18].length() == 0) ? null : Card.valueOf(elements[18])};
-                Card[] p3Hands = {Card.valueOf(elements[21]), Card.valueOf(elements[22]), (elements[23].length() == 0) ? null : Card.valueOf(elements[23]), (elements[24].length() == 0) ? null : Card.valueOf(elements[24]), (elements[25].length() == 0) ? null : Card.valueOf(elements[25])};
-                Card[] p4Hands = {Card.valueOf(elements[28]), Card.valueOf(elements[29]), (elements[30].length() == 0) ? null : Card.valueOf(elements[30]), (elements[31].length() == 0) ? null : Card.valueOf(elements[31]), (elements[32].length() == 0) ? null : Card.valueOf(elements[32])};
+                Card[] dealerCards = {Card.valueOf(elements[1]), Card.valueOf(elements[2]), (elements[3].length() == 0) ? null : Card.valueOf(elements[3]), (elements[4].length() == 0) ? null : Card.valueOf(elements[4]), (elements[5].length() == 0) ? null : Card.valueOf(elements[5])};
+                Card[] p1Cards = {Card.valueOf(elements[7]), Card.valueOf(elements[8]), (elements[9].length() == 0) ? null : Card.valueOf(elements[9]), (elements[10].length() == 0) ? null : Card.valueOf(elements[10]), (elements[11].length() == 0) ? null : Card.valueOf(elements[11])};
+                Card[] p2Cards = {Card.valueOf(elements[14]), Card.valueOf(elements[15]), (elements[16].length() == 0) ? null : Card.valueOf(elements[16]), (elements[17].length() == 0) ? null : Card.valueOf(elements[17]), (elements[18].length() == 0) ? null : Card.valueOf(elements[18])};
+                Card[] p3Cards = {Card.valueOf(elements[21]), Card.valueOf(elements[22]), (elements[23].length() == 0) ? null : Card.valueOf(elements[23]), (elements[24].length() == 0) ? null : Card.valueOf(elements[24]), (elements[25].length() == 0) ? null : Card.valueOf(elements[25])};
+                Card[] p4Cards = {Card.valueOf(elements[28]), Card.valueOf(elements[29]), (elements[30].length() == 0) ? null : Card.valueOf(elements[30]), (elements[31].length() == 0) ? null : Card.valueOf(elements[31]), (elements[32].length() == 0) ? null : Card.valueOf(elements[32])};
 
-                data.add(new DataModel(dealerHands[0].getIndex(), p1Hands[0].getIndex(), p1Hands[1].getIndex(), (p1Hands[2] == null) ? 0 : p1Hands[2].getIndex(), (p1Hands[3] == null) ? 0 : p1Hands[3].getIndex(), (p1Hands[4] == null) ? 0 : p1Hands[4].getIndex(), Result.Type.valueOf(elements[12])));
-                data.add(new DataModel(dealerHands[0].getIndex(), p2Hands[0].getIndex(), p2Hands[1].getIndex(), (p2Hands[2] == null) ? 0 : p2Hands[2].getIndex(), (p2Hands[3] == null) ? 0 : p2Hands[3].getIndex(), (p2Hands[4] == null) ? 0 : p2Hands[4].getIndex(), Result.Type.valueOf(elements[19])));
-                data.add(new DataModel(dealerHands[0].getIndex(), p3Hands[0].getIndex(), p3Hands[1].getIndex(), (p3Hands[2] == null) ? 0 : p3Hands[2].getIndex(), (p3Hands[3] == null) ? 0 : p3Hands[3].getIndex(), (p3Hands[4] == null) ? 0 : p3Hands[4].getIndex(), Result.Type.valueOf(elements[26])));
-                data.add(new DataModel(dealerHands[0].getIndex(), p4Hands[0].getIndex(), p4Hands[1].getIndex(), (p4Hands[2] == null) ? 0 : p4Hands[2].getIndex(), (p4Hands[3] == null) ? 0 : p4Hands[3].getIndex(), (p4Hands[4] == null) ? 0 : p4Hands[4].getIndex(), Result.Type.valueOf(elements[33])));
+                dataResult.add(new DataModelResult(dealerCards[0].getIndex(), p1Cards[0].getIndex(), p1Cards[1].getIndex(), (p1Cards[2] == null) ? 0 : p1Cards[2].getIndex(), (p1Cards[3] == null) ? 0 : p1Cards[3].getIndex(), (p1Cards[4] == null) ? 0 : p1Cards[4].getIndex(), Result.Type.valueOf(elements[12])));
+                dataResult.add(new DataModelResult(dealerCards[0].getIndex(), p2Cards[0].getIndex(), p2Cards[1].getIndex(), (p2Cards[2] == null) ? 0 : p2Cards[2].getIndex(), (p2Cards[3] == null) ? 0 : p2Cards[3].getIndex(), (p2Cards[4] == null) ? 0 : p2Cards[4].getIndex(), Result.Type.valueOf(elements[19])));
+                dataResult.add(new DataModelResult(dealerCards[0].getIndex(), p3Cards[0].getIndex(), p3Cards[1].getIndex(), (p3Cards[2] == null) ? 0 : p3Cards[2].getIndex(), (p3Cards[3] == null) ? 0 : p3Cards[3].getIndex(), (p3Cards[4] == null) ? 0 : p3Cards[4].getIndex(), Result.Type.valueOf(elements[26])));
+                dataResult.add(new DataModelResult(dealerCards[0].getIndex(), p4Cards[0].getIndex(), p4Cards[1].getIndex(), (p4Cards[2] == null) ? 0 : p4Cards[2].getIndex(), (p4Cards[3] == null) ? 0 : p4Cards[3].getIndex(), (p4Cards[4] == null) ? 0 : p4Cards[4].getIndex(), Result.Type.valueOf(elements[33])));
+
+                dataBust.add(new DataModelBust(p1Cards[0].getIndex(), p1Cards[1].getIndex(), (p1Cards[2] == null || p1Cards[3] == null) ? 0 : p1Cards[2].getIndex(), (p1Cards[3] == null || p1Cards[4] == null) ? 0 : p1Cards[3].getIndex(), 0, (getHandEval(p1Cards) > 21) ? CLASS_VALUES_BUST[1] : CLASS_VALUES_BUST[0]));
+                dataBust.add(new DataModelBust(p2Cards[0].getIndex(), p2Cards[1].getIndex(), (p2Cards[2] == null || p2Cards[3] == null) ? 0 : p2Cards[2].getIndex(), (p2Cards[3] == null || p2Cards[4] == null) ? 0 : p2Cards[3].getIndex(), 0, (getHandEval(p2Cards) > 21) ? CLASS_VALUES_BUST[1] : CLASS_VALUES_BUST[0]));
+                dataBust.add(new DataModelBust(p3Cards[0].getIndex(), p3Cards[1].getIndex(), (p3Cards[2] == null || p3Cards[3] == null) ? 0 : p3Cards[2].getIndex(), (p3Cards[3] == null || p3Cards[4] == null) ? 0 : p3Cards[3].getIndex(), 0, (getHandEval(p3Cards) > 21) ? CLASS_VALUES_BUST[1] : CLASS_VALUES_BUST[0]));
+                dataBust.add(new DataModelBust(p4Cards[0].getIndex(), p4Cards[1].getIndex(), (p4Cards[2] == null || p4Cards[3] == null) ? 0 : p4Cards[2].getIndex(), (p4Cards[3] == null || p4Cards[4] == null) ? 0 : p4Cards[3].getIndex(), 0, (getHandEval(p4Cards) > 21) ? CLASS_VALUES_BUST[1] : CLASS_VALUES_BUST[0]));
             });
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        int trainRatio = (int) (data.size() * 0.7);
-        List<DataModel> trainData = data.subList(0, trainRatio);
-        List<DataModel> evalData = data.subList(trainRatio, data.size());
+        int trainRatio = (int) (dataResult.size() * 0.7);
+        List<DataModelResult> trainData = dataResult.subList(0, trainRatio);
+        List<DataModelResult> evalData = dataResult.subList(trainRatio, dataResult.size());
 
         Instances trainArff = data2arff(trainData);
         Instances evalArff = data2arff(evalData);
@@ -89,12 +127,12 @@ public class Topse30050Model {
 //        Classifier modelJ48 =getBuiltClassifier(trainArff, new J48(), "-U");
 //        evalResult(modelJ48, evalArff);
 //        System.out.println();
-
-        System.out.println("***** RandomForest *****");
-        Classifier modelRf = getBuiltClassifier(trainArff, new RandomForest());
-        evalResult(modelRf, evalArff);
-        System.out.println();
-
+//
+//        System.out.println("***** RandomForest *****");
+//        Classifier modelRf = getBuiltClassifier(trainArff, new RandomForest());
+//        evalResult(modelRf, evalArff);
+//        System.out.println();
+//
 //        System.out.println("***** Logistic *****");
 //        Classifier modelLogi = getBuiltClassifier(trainArff, new Logistic());
 //        evalResult(modelLogi, evalArff);
@@ -109,25 +147,77 @@ public class Topse30050Model {
 //        Classifier modelMP = getBuiltClassifier(trainArff, new MultilayerPerceptron(), "-L", "0.5", "-M", "0.1");
 //        evalResult(modelMP, evalArff);
 //        System.out.println();
+
+        System.out.println("***** SVM *****");
+        Classifier modelSVM = getBuiltClassifier(trainArff, new LibSVM());
+        evalResult(modelSVM, evalArff);
+        System.out.println();
+
+
+        aiModelResult = modelSVM;
+        try {
+            SerializationHelper.write(CLASSIFIER_MODEL_RESULT, aiModelResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        List<DataModelBust> trainDataBust = dataBust.subList(0, trainRatio);
+        List<DataModelBust> evalDataBust = dataBust.subList(trainRatio, dataBust.size());
+
+        trainArff = dataBust2arff(trainDataBust);
+        evalArff = dataBust2arff(evalDataBust);
+//        writeArff(trainArff, TRAIN_ARFF);
+//        writeArff(evalArff, EVAL_ARFF);
+
+//        System.out.println("***** NaiveBayes *****");
+//        Classifier modelNb = getBuiltClassifier(trainArff, new NaiveBayes());
+//        evalResult(modelNb, evalArff);
+//        System.out.println();
 //
-//        System.out.println("***** SVM *****");
-//        Classifier modelSVM = getBuiltClassifier(trainArff, new LibSVM());
-//        evalResult(modelSVM, evalArff);
+//        System.out.println("***** J48 *****");
+//        modelJ48 =getBuiltClassifier(trainArff, new J48(), "-U");
+//        evalResult(modelJ48, evalArff);
+//        System.out.println();
+//
+//        System.out.println("***** RandomForest *****");
+//        modelRf = getBuiltClassifier(trainArff, new RandomForest());
+//        evalResult(modelRf, evalArff);
+//        System.out.println();
+//
+//        System.out.println("***** Logistic *****");
+//        Classifier modelLogi = getBuiltClassifier(trainArff, new Logistic());
+//        evalResult(modelLogi, evalArff);
+//        System.out.println();
+//
+//        System.out.println("***** SMO *****");
+//        Classifier modelSMO = getBuiltClassifier(trainArff, new SMO());
+//        evalResult(modelSMO, evalArff);
+//        System.out.println();
+//
+//        System.out.println("***** Multilayer Perceptron *****");
+//        Classifier modelMP = getBuiltClassifier(trainArff, new MultilayerPerceptron(), "-L", "0.5", "-M", "0.1");
+//        evalResult(modelMP, evalArff);
 //        System.out.println();
 
+        System.out.println("***** SVM *****");
+        modelSVM = getBuiltClassifier(trainArff, new LibSVM());
+        evalResult(modelSVM, evalArff);
+        System.out.println();
 
-        aiModel = modelRf;
+
+        aiModelBust = modelSVM;
         try {
-            SerializationHelper.write(CLASSIFIER_MODEL, aiModel);
+            SerializationHelper.write(CLASSIFIER_MODEL_BUST, aiModelBust);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 
-    private static Instances data2arff(List<DataModel> data) {
+    private static Instances data2arff(List<DataModelResult> data) {
         FastVector classValues = new FastVector();
-        for (Result.Type classValue : CLASS_VALUES) {
+        for (Result.Type classValue : CLASS_VALUES_RESULT) {
             classValues.addElement(classValue.name());
         }
 
@@ -152,6 +242,36 @@ public class Topse30050Model {
             values[4] = d.hand4;
             values[5] = d.hand5;
             values[6] = arff.attribute(6).indexOfValue(d.result.name());
+            arff.add(new Instance(1.0, values));
+        });
+
+        return arff;
+    }
+    private static Instances dataBust2arff(List<DataModelBust> data) {
+        FastVector classValues = new FastVector();
+        for (String classValue : CLASS_VALUES_BUST) {
+            classValues.addElement(classValue);
+        }
+
+        FastVector attributes = new FastVector();
+        attributes.addElement(new Attribute("Hand 1"));
+        attributes.addElement(new Attribute("Hand 2"));
+        attributes.addElement(new Attribute("Hand 3"));
+        attributes.addElement(new Attribute("Hand 4"));
+        attributes.addElement(new Attribute("Hand 5"));
+        attributes.addElement(new Attribute("IS BUST", classValues));
+
+        Instances arff = new Instances("Data", attributes, 0);
+        arff.setClassIndex(arff.numAttributes() - 1);
+
+        data.forEach(d -> {
+            double[] values = new double[arff.numAttributes()];
+            values[0] = d.hand1;
+            values[1] = d.hand2;
+            values[2] = d.hand3;
+            values[3] = d.hand4;
+            values[4] = d.hand5;
+            values[5] = arff.attribute(5).indexOfValue(d.isBustStr);
             arff.add(new Instance(1.0, values));
         });
 
@@ -182,7 +302,6 @@ public class Topse30050Model {
         return classifier;
     }
 
-
     private static void evalResult(Classifier c, Instances arff) {
         try {
             Evaluation eval = new Evaluation(arff);
@@ -193,30 +312,49 @@ public class Topse30050Model {
             e.printStackTrace();
         }
     }
+
+
     static Result.Type getPredictedResult(int dealerUpCard, int hand1, int hand2, int hand3, int hand4, int hand5) {
-        if (!new File(CLASSIFIER_MODEL).exists()) {
+        if (!new File(CLASSIFIER_MODEL_RESULT).exists()) {
             makeClassifierModel();
         }
 
         int predictedValue = Integer.MAX_VALUE;
         try {
-            if (aiModel == null) {
-                aiModel = (Classifier) SerializationHelper.read(CLASSIFIER_MODEL);
+            if (aiModelResult == null) {
+                aiModelResult = (Classifier) SerializationHelper.read(CLASSIFIER_MODEL_RESULT);
             }
-            List<DataModel> predictData = new ArrayList<>();
-            predictData.add(new DataModel(dealerUpCard, hand1, hand2, hand3, hand4, hand5, Result.Type.WIN)); // 結果の引数はダミー
+            List<DataModelResult> predictData = new ArrayList<>();
+            predictData.add(new DataModelResult(dealerUpCard, hand1, hand2, hand3, hand4, hand5, Result.Type.WIN)); // 結果の引数はダミー
             Instances predictArff = data2arff(predictData);
-            predictedValue = (int) new Evaluation(predictArff).evaluateModelOnce(aiModel, predictArff.firstInstance());
+            predictedValue = (int) new Evaluation(predictArff).evaluateModelOnce(aiModelResult, predictArff.firstInstance());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return (predictedValue == Integer.MAX_VALUE) ? null : CLASS_VALUES[predictedValue];
+        return (predictedValue == Integer.MAX_VALUE) ? null : CLASS_VALUES_RESULT[predictedValue];
+    }
+    static Boolean isBust(int hand1, int hand2, int hand3, int hand4, int hand5) {
+        if (!new File(CLASSIFIER_MODEL_BUST).exists()) {
+            makeClassifierModel();
+        }
+
+        int predictedValue = Integer.MAX_VALUE;
+        try {
+            if (aiModelBust == null) {
+                aiModelBust = (Classifier) SerializationHelper.read(CLASSIFIER_MODEL_BUST);
+            }
+            List<DataModelBust> predictData = new ArrayList<>();
+            predictData.add(new DataModelBust(hand1, hand2, hand3, hand4, hand5, CLASS_VALUES_BUST[0])); // 結果の引数はダミー
+            Instances predictArff = dataBust2arff(predictData);
+            predictedValue = (int) new Evaluation(predictArff).evaluateModelOnce(aiModelBust, predictArff.firstInstance());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (predictedValue == Integer.MAX_VALUE) ? null : predictedValue == 1;
     }
 
 
     public static void main(String[] args) {
         makeClassifierModel();
-//        Card nextCard = Topse30050Model.getPredictedResult(7, 1, 2, 0, 0, 0, 3);
-//        System.out.println(nextCard);
     }
 }
